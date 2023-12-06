79a3f6d73f9e215c8e9617294c89b6889aa8a719
siddarthkoul <siddarthkoul21@gmail.com>
2023-12-06 23:10:58 +0530

    NavBar Updations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
 import './App.css';
 import Home from './screens/Home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './screens/Login';
 
 function App() {
   return (
-    <>
-    <div> <Home /> </div>
-    </>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path = "/" element = {<Home/>}/>
+        <Route exact path = "/login" element = {<Login/>}/>
+      </Routes>
+    </div>
+    </Router>
   );
 }
 
