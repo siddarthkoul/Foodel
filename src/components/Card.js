3e6664818aa53c4cffc0f2a544f27cb96e31fe60
siddarthkoul <siddarthkoul21@gmail.com>
2023-12-07 22:11:10 +0530

    added routes, carousel

---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Card() {
+   return (
+      <div>
+         <div>
+            <div
+               className="card mt-3"
+               style={{ width: "18rem", maxHeight: "360px" }}>
+               <img src="..." className="card-img-top" alt="..." />
+               <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                     Some important information about Card
+                  </p>
+                  <div className="container ">
+                     <select className="m-2 h-100  bg-warning rounded">
+                        {Array.from(Array(6), (e, i) => {
+                           return (
+                              <option key={i + 1} value={i + 1}>
+                                 {i + 1}{" "}
+                              </option>
+                           );
+                        })}
+                     </select>
+
+                     <select className="m-2 h-100  bg-warning rounded">
+                        <option value="half">Half</option>
+                        <option value="full">full</option>
+                     </select>
+                     <div className='d-inline h-100 fs-5'>
+                        Total Price
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousal from "../components/Carousal";
 
 export default function Home() {
-  return (
-    <div>
-
-    <div> <Navbar /> </div>
-    <div> body </div>
-    <div> <Footer /> </div>
-    
-    </div>
-  )
+   return (
+      <div>
+         <div>
+            <Navbar />
+         </div>
+         <div>
+        <Carousal/>
+        </div>
+         <div>
+            <Card/>
+         </div>
+         <div>
+            <Footer />
+         </div>
+      </div>
+   );
 }
- 
