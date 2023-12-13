a9f8a516ec4f1dbf4294cacef3070468aa53eaac
siddarthkoul <siddarthkoul21@gmail.com>
2023-12-13 13:25:17 +0530

    Created schema and updated Db.js

--- backend/db.js
+++ backend/db.js
@@ -2,14 +2,25 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://admin:admin123@cluster0.aytrepb.mongodb.net/?retryWrites=true&w=majority`, {
-      useNewUrlParser: true,
-    });
-    console.log(`MongoDB Connected: {conn.connection.host}`);
+    const conn = await mongoose.connect(
+      "mongodb+srv://admin:admin123@cluster0.aytrepb.mongodb.net/foodel?retryWrites=true&w=majority"
+    );
+
+    if (conn) {
+      console.log(`MongoDB Connected`);
+      
+      // Fetch data here
+      const fetched_data = mongoose.connection.db.collection("foodel");
+      const data = await fetched_data.find({}).toArray();
+      console.log();
+    } else {
+      console.error("Failed to connect");
+      process.exit(1);
+    }
   } catch (error) {
     console.error(error.message);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
