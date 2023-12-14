d1438576ca3e55c2363f475482432203592b77f0
siddarthkoul <siddarthkoul21@gmail.com>
2023-12-15 00:23:51 +0530

    Added Validations

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 5000
 const connectDB = require('./db')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
-// express
-//body parser
+// app.use((req, res, next)=> {
+//   res.setHeader("Access-Control-ALlow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Headers",
+//   "Orgin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
+app.use(cors());
 app.use(express.json());
 app.use('/api', require("./Routes/CreateUser"));
 app.get('/', (req, res) => {
